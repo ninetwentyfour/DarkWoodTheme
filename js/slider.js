ee61222f9311c62c7126904632e66b05c79d57d5
Travis Berry <travisberry@travisberry.com>
2009-12-20 12:32:30 -0700

    Added image fade in to slider.js

--- js/slider.js
+++ js/slider.js
@@ -127,3 +127,36 @@ $(function() {
 	});
 	
 });
+
+document.write("<style type='text/css'>#thephoto {visibility:hidden;}</style>");
+
+function initImage() {
+	imageId = 'thephoto';
+	image = document.getElementById(imageId);
+	setOpacity(image, 0);
+	image.style.visibility = "visible";
+	fadeIn(imageId,0);
+}
+function fadeIn(objId,opacity) {
+	if (document.getElementById) {
+		obj = document.getElementById(objId);
+		if (opacity <= 100) {
+			setOpacity(obj, opacity);
+			opacity += 10;
+			window.setTimeout("fadeIn('"+objId+"',"+opacity+")", 100);
+		}
+	}
+}
+function setOpacity(obj, opacity) {
+	opacity = (opacity == 100)?99.999:opacity;
+	// IE/Win
+	obj.style.filter = "alpha(opacity:"+opacity+")";
+	// Safari<1.2, Konqueror
+	obj.style.KHTMLOpacity = opacity/100;
+	// Older Mozilla and Firefox
+	obj.style.MozOpacity = opacity/100;
+	// Safari 1.2, newer Firefox and Mozilla, CSS3
+	obj.style.opacity = opacity/100;
+}
+window.onload = function() {initImage()}
+// 
