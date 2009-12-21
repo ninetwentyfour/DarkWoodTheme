24c24bc8a72766913974672812dc321fadcd67ca
Travis Berry <travisberry@travisberry.com>
2009-12-20 20:11:15 -0700

    Removed google code from 404 page

--- 404.php
+++ 404.php
@@ -248,6 +248,7 @@ if(!aa_print_html($AA_STATUS_CODE)){
 
 </div>
 <div id="footer">
+Dark Wood Theme Created By <a href="http://www.travisberry.com/">Travis Berry</a>
 
 </div>
 </div>
@@ -255,18 +256,7 @@ if(!aa_print_html($AA_STATUS_CODE)){
 
 
 
-<!--[if lt IE 8]>
-<script src="http://ie7-js.googlecode.com/svn/version/2.0(beta3)/IE8.js" type="text/javascript"></script>
-<![endif]-->
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-11593601-1");
-pageTracker._trackPageview();
-} catch(err) {}</script>
+
 </body>
 </html>
 <?php } 
