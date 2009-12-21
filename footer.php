22ce0d75ee4644817879efda033328164558ebca
Travis Berry <travisberry@travisberry.com>
2009-12-20 20:06:54 -0700

    removed google ie code

--- footer.php
+++ footer.php
@@ -8,10 +8,5 @@ Dark Wood Theme Created By <a href="http://www.travisberry.com/">Travis Berry</a
 </div>
 
 
-
-<!--[if lt IE 8]>
-<script src="http://ie7-js.googlecode.com/svn/version/2.0(beta3)/IE8.js" type="text/javascript"></script>
-<![endif]-->
-
 </body>
 </html>
