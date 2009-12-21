1bf5c7397df010c6a0a39a653b2d6c954c43c51b
Travis Berry <travisberry@travisberry.com>
2009-12-20 20:05:06 -0700

    Removed analytics code

--- footer.php
+++ footer.php
@@ -12,14 +12,6 @@ Dark Wood Theme Created By <a href="http://www.travisberry.com/">Travis Berry</a
 <!--[if lt IE 8]>
 <script src="http://ie7-js.googlecode.com/svn/version/2.0(beta3)/IE8.js" type="text/javascript"></script>
 <![endif]-->
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-11111111-1");
-pageTracker._trackPageview();
-} catch(err) {}</script>
+
 </body>
 </html>
