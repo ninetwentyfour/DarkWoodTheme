549090550df2c1c9fd1a9eb01cb24ef93a2d496e
Travis Berry <travisberry@travisberry.com>
2009-12-20 20:14:52 -0700

    Added wp_footer to footer

--- 404.php
+++ 404.php
@@ -249,7 +249,7 @@ if(!aa_print_html($AA_STATUS_CODE)){
 </div>
 <div id="footer">
 Dark Wood Theme Created By <a href="http://www.travisberry.com/">Travis Berry</a>
-
+<?php wp_footer(); ?>
 </div>
 </div>
 </div>
--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
         || !dynamic_sidebar() ) : ?>
 
 Dark Wood Theme Created By <a href="http://www.travisberry.com/">Travis Berry</a>
+<?php wp_footer(); ?>
 </div>
 </div>
 </div>
