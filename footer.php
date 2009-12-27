89834693e0046768c189d0d01394820f12baaccd
Travis Berry <travisberry@travisberry.com>
2009-12-27 15:23:52 -0700

    Removed widget bs from footer.

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 <div id="footer">
-    <?php if ( !function_exists('dynamic_sidebar')
-        || !dynamic_sidebar() ) : ?>
+    
 
 Dark Wood Theme Created By <a href="http://www.travisberry.com/">Travis Berry</a>
 <?php wp_footer(); ?>
